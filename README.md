3325699523f96d6f0ae90baed12a5ccc77c87c44
Alexander Bandeira <73051285+devbandeira@users.noreply.github.com>
2024-12-09 15:23:11 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação total do projeto https://www.tabnews.com.br do "zero" ao concluído para aprendizado.
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 function Home() {
-
-    return <h1>Agatha, Eu amo muito voce ♥</h1>
-
+  return <h1>Agatha, Eu amo muito voce ♥</h1>;
 }
 
+function CARAI() {
+  return <h2>isso ai</h2>;
+}
 
 export default Home;
